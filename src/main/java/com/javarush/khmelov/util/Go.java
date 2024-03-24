551fd55904ab9b4b651ee4f27de35c41fa2715bb
Egor Kondrashov <erop.kondrashov@yandex.ru>
2024-03-24 20:04:12 +0400

    comment some lines in Go class

--- src/main/java/com/javarush/khmelov/util/Go.java
+++ src/main/java/com/javarush/khmelov/util/Go.java
@@ -2,11 +2,11 @@ package com.javarush.khmelov.util;
 
 @SuppressWarnings("unused")
 public class Go {
-    public static final String INDEX = "";
-    public static final String HOME = "/home";
-
-    public static final String SIGNUP = "/signup";
-    public static final String LOGIN = "/login";
+//    public static final String INDEX = "";
+//    public static final String HOME = "/home";
+//
+//    public static final String SIGNUP = "/signup";
+//    public static final String LOGIN = "/login";
     public static final String LOGOUT = "/logout";
     public static final String PROFILE = "/profile";
     public static final String LIST_USER = "/list-user";
