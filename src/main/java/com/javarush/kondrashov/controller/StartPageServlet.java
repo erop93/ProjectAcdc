cb54fb900402f03ffaefc7a5b44dc7c331a519cf
Egor Kondrashov <erop.kondrashov@yandex.ru>
2024-03-25 23:13:50 +0400

    add User class, add StartPageServlet, add start.jsp page

--- src/main/java/com/javarush/kondrashov/controller/StartPageServlet.java
+++ src/main/java/com/javarush/kondrashov/controller/StartPageServlet.java
@@ -1,2 +1,17 @@
-package com.javarush.kondrashov.controller;public class StartPageServlet {
+package com.javarush.kondrashov.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("")
+public class StartPageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }
--- src/main/java/com/javarush/kondrashov/entity/User.java
+++ src/main/java/com/javarush/kondrashov/entity/User.java
@@ -1,2 +1,30 @@
-package com.javarush.kondrashov.entity;public class User {
+package com.javarush.kondrashov.entity;
+
+public class User {
+
+    Long id;
+
+    String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
 }
