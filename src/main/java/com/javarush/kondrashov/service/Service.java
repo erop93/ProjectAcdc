9a2a39b4d3d423989da13c340890ed9d153a0476
Egor Kondrashov <erop.kondrashov@yandex.ru>
2024-03-26 15:20:01 +0400

    add Service class, BaseRepository interface and UserRepository class

--- src/main/java/com/javarush/kondrashov/service/Service.java
+++ src/main/java/com/javarush/kondrashov/service/Service.java
@@ -1,14 +1,14 @@
-package com.javarush.kondrashov.repository;
+package com.javarush.kondrashov.service;
 
 import com.javarush.kondrashov.entity.User;
 
 import java.util.Collection;
 import java.util.Optional;
 
-public interface BaseRepository {
+public interface Service {
     void create(User user);
     void update(User user);
     void delete(User user);
-    Optional<User> get(long id);
-    Collection<User> getAll();
+    Optional<User> getAll();
+    Collection<User> get (long id);
 }
